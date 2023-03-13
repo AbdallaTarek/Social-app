215dbfcfd9e9828f159bfb0f049daca393a3d43f
AbdallaTarek <abdalla.taarek@gmail.com>
2023-03-13 11:27:25 +0200

    Initial Commit

---
+++ Social/SocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialApp.swift
+//  Social
+//
+//  Created by abdullah tarek on 13/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
